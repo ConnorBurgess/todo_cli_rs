6bc40c215796ca03b2a115dc4ab043ae24fca047
connorburgess <76454557+ConnorBurgess@users.noreply.github.com>
2023-11-17 02:47:12 +0700

    Add clap to depedencies, implemment cli command for add with placeholder cout

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
-mod tasks;
+use clap::{Parser, Subcommand};
+use std::io;
 
-fn main() {
-    println!("Hello, world!");
+mod tasks;
+use tasks::Task;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Adds a new task
+    Add {
+        description: String,
+    },
+}
+
+fn main() -> io::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add { description } => {
+            println!("Added a new task");
+            // Call function to add task; add_task(description);
+        }
+    }
+
+    Ok(())
 }
