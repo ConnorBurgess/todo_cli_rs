28f44be6bd89214894e57f0049e0b829488daafe
connorburgess <76454557+ConnorBurgess@users.noreply.github.com>
2023-11-16 18:09:23 +0700

    Add serde and serde_json to cargo.toml to serialize and deserialize data, add a Task struct to tasks.rs with method to create new task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod tasks;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/tasks.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Task {
+    pub description: String,
+    pub completed: bool,
+}
+
+impl Task {
+    pub fn new(description: String) -> Task {
+        Task {
+            description,
+            completed: false,
+        }
+    }
+}
